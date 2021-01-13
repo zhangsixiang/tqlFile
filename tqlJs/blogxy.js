e5026b25d5787c82e0c6aa8c533bc94dba5219af
踏雕上月 <961051447@qq.com>
2021-01-14 07:46:56 +0800

    Add files via upload

---
+++ tqlJs/blogxy.js
@@ -0,0 +1,21 @@
+var url = window.location.hostname;
+var system = {};
+var p = navigator.platform;
+system.win = p.indexOf("Win") == 0;
+system.mac = p.indexOf("Mac") == 0;
+system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+if(system.win){
+	$.getScript('https://ip.ws.126.net/ipquery', function(){  
+		if(localAddress.city.indexOf('北京') != -1 || localAddress.city.indexOf('南京') != -1){
+		  window.location.href = 'https://blog.zsxcool.com/404.html';      
+		}else{        
+		 return;
+		}
+		console.log(localAddress.province)    
+		if(localAddress.province.indexOf('北京市') != -1 || localAddress.province.indexOf('南京市') != -1){
+		  window.location.href = 'https://blog.zsxcool.com/404.html';      
+		}else{        
+		 return;      
+		}
+	});
+}
